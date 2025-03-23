b8c8d0a0e117deebc678651e507634c616441263
ethnpan07 <ethanpan@cpp.edu>
2025-03-22 21:31:26 -0700

    start build page for infodump

--- BizNerds/src/InfoDump.tsx
+++ BizNerds/src/InfoDump.tsx
@@ -7,17 +7,37 @@ import {
   } from "@/components/ui/carousel"
 import { Card, CardContent } from "./components/ui/card"
 
+import * as React from "react"
+import Autoplay from "embla-carousel-autoplay"
+
 export default function InfoDump() {
+    const cardData = [
+        { title: "Index Funds", description: "This is the first card This is the first card This is the first card This is the first card This is the first card" },
+        { title: "Stocks", description: "This is the second card" },
+        { title: "Bonds", description: "This is the third card" },
+        { title: "Real Estate", description: "This is the fourth card" },
+        { title: "High Interest Accounts ", description: "This is the fifth card" },
+      ];
+
+      const plugin = React.useRef(
+        Autoplay({ delay: 4000, stopOnInteraction: true })
+      )
 
   return (
-    <div className="flex flex-col items-center gap-4">
-      <Carousel className="w-full max-w-xs">
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-semibold">Investing 101</h1>
+      <Carousel
+      plugins={[plugin.current]}
+      className="w-full max-w-7/8 h-full p-8"
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
+    >
       <CarouselContent>
         {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index}>
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
+          <CarouselItem className="h-full" key={index}>
+            <div className="p-1 h-full">
+              <Card className="h-full">
+                <CardContent className="flex items-center h-full justify-center p-6">
                   <span className="text-4xl font-semibold">{index + 1}</span>
                 </CardContent>
               </Card>
@@ -27,8 +47,9 @@ export default function InfoDump() {
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
-    </Carousel>
-
+    </Carousel> 
     </div>
+    
   )
 }
+
